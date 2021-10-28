ec72d0e93fc366de5a896d700810979fbc7956fd
Mykman <61824558+Alienxbe@users.noreply.github.com>
2021-10-28 22:32:30 +0200

    Update monitoring.sh

--- monitoring.sh
+++ monitoring.sh
@@ -1,32 +1,38 @@
 #!/bin/bash
 
+ARCHITECTURE=$(uname -a)
+CPU_PHYS=$(lscpu| awk '/^CPU\(s\)/ {print $2}')
+CPU_VIRT=$(cat /proc/cpuinfo | grep processor | wc -l)
+CPU_LOAD=$(uptime | awk '{print $9}')
 
-MEMTOTAL=$(awk '/MemTotal/ {print $2}' /proc/meminfo) 
+MEM_TOT=$(free -m | awk '/Mem/ {print $2}')
+MEM_USED=$(free -m | awk '/Mem/ {print $3}')
 
-MEMAVAIL=$(awk '/MemAvail/ {print $2}' /proc/meminfo)
+DISK_TOT=$(df -h --total | awk '/total/ {print $2}')
+DISK_USED=$(df -h --total | awk '/total/ {print $3}')
+DISK_PCT=$(df -h --total | awk '/total/ {print $5}')
 
-MEMUSED=$((MEMTOTAL - MEMAVAIL))
+LAST_BOOT=$(who -b | awk '{print $3" "$4}')
+LVM=$(lsblk| awk '/root/ {print $6}')
+TCP_CONNECTION=$(ss -s | awk '/TCP:/ {print $2}')
+SUDO_COUNT=$(expr $(cat /var/log/sudo/logs42 | wc -l) / 2)
 
-LVM=$(lsblk | awk '/root/ {print $6}')
-
-
-wall << End_Of_Message
-	#Architecture: `uname -a`
-	#CPU physical: `lscpu | awk '/^CPU\(s\)/ {print $2}'`
-	#vCPU : `grep -c 'processor' /proc/cpuinfo`
-	`echo $MEMUSED $MEMTOTAL | awk '{printf "#Memory Usage: %d/%dMB (%.2f%%)\n", ($1/1024), ($2/1024), ($1/$2)*100}'`
-	`df --total | tail -n 1 | awk '{printf "#Disk Usage: %d/%dGb (%d%%)\n", ($3/1024), ($2/1048576), $5}'`
-	#CPU load: "`top -bn1 | grep '^%Cpu' | cut -c 9- | xargs | awk '{printf("%.1f%%"), $1 + $3}'`"
-	#Last boot: `who -b | awk '{print $3" "$4}'`
+wall <<	End_Of_Message
+	#Architecture: $ARCHITECTURE 
+	#CPU: $CPU_PHYS
+	#vCPU: $CPU_VIRT
+	#Memory usage: `echo $MEM_USED $MEM_TOT | awk '{printf "%d/%dMB (%.2f%%)\n", $(1), $(2), ($1/$2)*100}'`
+	#Disk usage: $DISK_USED/$DISK_TOT ($DISK_PCT)
+	#CPU load: `echo ${CPU_LOAD::-1} | awk '{printf "%d%%\n", ($1)*100}'`
+	#Last boot: $LAST_BOOT
 	#LVM use: `if [ "$LVM" = "lvm" ];
 	then
 		echo "yes"
 	else
 		echo "no"
 	fi`
-	`awk '$4=="01" {count++} END{printf "#Connexions TCP : %d ESTABLISHED\n", count}' /proc/net/tcp`
-	#User log: `who | awk '{print $1}' | uniq | wc -l`
-	`ip -br a show $(ip route show default | awk '{print $5}') | sed 's/\/[[:digit:]]\{1,3\}//g' | awk '{printf "#Network: IP %s (%s)\n", $3, $4}'`
-	#Sudo: `journalctl _COMM=sudo | grep COMMAND | wc -l` cmd
-
+	#TCP connection: $TCP_CONNECTION ESTABLISHED
+	#User log: `who | wc -l`
+	`ip -br a show $(ip route show default | awk '{print $5}') | sed 's/\/[[:digit:]]\{1,3\}//g' | awk '{printf "#Network: IP %s (%s)\n", $3, $4}'`	
+	#Sudo: $SUDO_COUNT
 End_Of_Message
